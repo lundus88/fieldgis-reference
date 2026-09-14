72fbecc1a88693529c6c8610115cd168d8805d93
lundus88 <tukangsukat@gmail.com>
2026-09-15 06:53:03 +0800

    LOM 6.9.1: add strict promotion digest regressions

--- vl/lom-6-8-governed-pr-evidence-promotion/test_promotion.py
+++ vl/lom-6-8-governed-pr-evidence-promotion/test_promotion.py
@@ -1,6 +1,9 @@
 import unittest
 from promotion import PromotionRequest, build_promotion_package, manifest_digest, validate_promotion
 
+D1='1'*64
+D2='2'*64
+
 
 def req(**overrides):
     base = dict(
@@ -10,7 +13,8 @@ def req(**overrides):
         evidence_sha='c'*40,
         source_reference='ci://lom-6-7/run/1',
         validation_status='PREPARE_PR',
-        rollback_plan_digest='rollback-digest-1234567890',
+        validation_package_sha256=D1,
+        rollback_plan_digest=D2,
         target_action='NON_PRODUCTION_REVERSIBLE_ACTION',
         production=False,
         risk='LOW',
@@ -44,8 +48,17 @@ class PromotionGateTests(unittest.TestCase):
     def test_non_prepare_validation_holds(self):
         self.assertEqual(validate_promotion(req(validation_status='HOLD'))['reason'], 'SANDBOX_VALIDATION_REQUIRED')
 
+    def test_missing_validation_package_digest_holds(self):
+        self.assertEqual(validate_promotion(req(validation_package_sha256=''))['reason'], 'VALIDATION_PACKAGE_DIGEST_REQUIRED')
+
+    def test_non_sha256_validation_package_digest_holds(self):
+        self.assertEqual(validate_promotion(req(validation_package_sha256='x'*64))['reason'], 'VALIDATION_PACKAGE_DIGEST_REQUIRED')
+
     def test_missing_rollback_holds(self):
-        self.assertEqual(validate_promotion(req(rollback_plan_digest=''))['reason'], 'ROLLBACK_EVIDENCE_REQUIRED')
+        self.assertEqual(validate_promotion(req(rollback_plan_digest=''))['reason'], 'ROLLBACK_PLAN_DIGEST_REQUIRED')
+
+    def test_non_sha256_rollback_holds(self):
+        self.assertEqual(validate_promotion(req(rollback_plan_digest='rollback-digest-1234567890'))['reason'], 'ROLLBACK_PLAN_DIGEST_REQUIRED')
 
     def test_unknown_risk_holds(self):
         self.assertEqual(validate_promotion(req(risk='UNKNOWN'))['reason'], 'UNKNOWN_RISK')
@@ -65,11 +78,21 @@ class PromotionGateTests(unittest.TestCase):
     def test_manifest_digest_changes_with_candidate(self):
         self.assertNotEqual(manifest_digest(req()), manifest_digest(req(candidate_sha='d'*40)))
 
+    def test_manifest_digest_changes_with_validation_package(self):
+        self.assertNotEqual(manifest_digest(req()), manifest_digest(req(validation_package_sha256='3'*64)))
+
+    def test_manifest_digest_changes_with_rollback_plan(self):
+        self.assertNotEqual(manifest_digest(req()), manifest_digest(req(rollback_plan_digest='4'*64)))
+
     def test_package_preserves_human_merge_boundary(self):
         package = build_promotion_package(req())
         self.assertTrue(package['pr_candidate']['human_merge_required'])
         self.assertEqual(package['auto_merge'], 'DISABLED')
         self.assertEqual(package['production_execution'], 'DISABLED')
+        self.assertEqual(package['schema'], 'lom.pr-evidence-promotion-package/2')
+        self.assertEqual(package['manifest']['schema'], 'lom.pr-evidence-promotion/2')
+        self.assertEqual(package['evidence_chain']['validation_package_sha256'], D1)
+        self.assertEqual(package['evidence_chain']['rollback_plan_sha256'], D2)
         self.assertEqual(len(package['manifest']['manifest_sha256']), 64)
 
 
